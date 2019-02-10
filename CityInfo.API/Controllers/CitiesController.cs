c495d3f0d6f6fe3e7a7af13d61b58e099cd34bd5
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-02-10 10:17:28 +0000

    Why returning IActionResult and using helper methods to return data is a good idea

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        // returns "200 OK" with "null" response - clearly not RESTful 
+        // better alrternative is to return "IActionResult" and use OK()/NotFound() helper methods so correct status codes are returned
+        // and also content negotiation sia automatically handled by MVC middleware
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == id));
+        }
+    }
+}
