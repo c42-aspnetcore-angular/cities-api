51469f6cc8051151b7e52bc6edb8f059bc4dba7b
c42-arun <cloud.fortytwo@gmail.com>
2019-02-11 18:28:49 +0000

    Entities & DbContext added. DB created if not exists

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Entities;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
@@ -40,6 +42,9 @@ namespace CityInfo.API
             //});
 
             services.AddTransient<IMailService, CloudMailService>();
+
+            var connectionString = Configuration["connectionStrings:cityInfoDBConnectionString"];
+            services.AddDbContext<CityInfoContext>(o => o.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
